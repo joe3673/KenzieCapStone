66c2ba217344cb5b5cd188e82c0f472a76f6899d
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2023-12-05 08:35:34 -0500

    Add files via upload

---
+++ Application/src/main/java/com/kenzie/appserver/controller/EventController.java
@@ -0,0 +1,9 @@
+package com.kenzie.appserver.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/Event")
+public class EventController {
+}
---
+++ Application/src/main/java/com/kenzie/appserver/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.kenzie.appserver.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/User")
+public class UserController {
+}
