93af5966e0698270bfd2932a4d8a277385e73f93
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2023-12-11 00:08:39 -0500

    Updated UserRecord

--- Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
@@ -7,49 +7,105 @@ import com.kenzie.appserver.service.model.Event;
 import java.util.List;
 
 public class UserRecord{
-
     private final String userID;
-
     private String userName;
-
     private String password;
+    private List<String> eventsList;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private List<String> notifications;
+    private String userType;
 
-    private List<Event> eventsList;
 
-
-    public UserRecord(String userID, String userName, String password, List<Event> eventsList) {
+    public UserRecord(String userID, String userName, String password, List<String> eventsList, String email, String firstName, String lastName, List<String> notifications, String userType) {
         this.userID = userID;
         this.userName = userName;
         this.password = password;
         this.eventsList = eventsList;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.notifications = notifications;
+        this.userType = userType;
     }
+
     @DynamoDBHashKey(attributeName = "ID")
     public String getUserID(){
         return userID;
     }
+
     @DynamoDBAttribute(attributeName = "Username")
     public String getUserName() {
         return userName;
     }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
     @DynamoDBAttribute(attributeName = "Password")
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password){
+        this.password = password;
+    }
     @DynamoDBAttribute(attributeName = "EventList")
-    public List<Event> getEventsList() {
+    public List<String> getEventsList() {
         return eventsList;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEventsList(List<Event> eventsList) {
+    public void setEventsList(List<String> eventsList){
         this.eventsList = eventsList;
     }
+
+    @DynamoDBAttribute(attributeName = "Email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(){
+        this.email = email;
+    }
+
+
+    @DynamoDBAttribute(attributeName = "First_Name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    @DynamoDBAttribute(attributeName = "Last_Name")
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    @DynamoDBAttribute(attributeName = "Notifications")
+    public List<String> getNotifications(){
+        return notifications;
+    }
+
+    public void setNotifications(List<String> notifications){
+        this.notifications = notifications;
+    }
+
+    @DynamoDBAttribute(attributeName = "User_Type")
+    public String getUserType(){
+        return userType;
+    }
+
+    public void setUserType(String userType){
+        this.userType = userType;
+    }
+
+
 }
