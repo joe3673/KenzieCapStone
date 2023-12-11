0ed657c4914c9ab634d73672d2cfbb4351358280
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2023-12-11 00:10:59 -0500

    Updated UserResponse

--- Application/src/main/java/com/kenzie/appserver/controller/model/UserResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/UserResponse.java
@@ -1,4 +1,86 @@
 package com.kenzie.appserver.controller.model;
 
-public class UserResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kenzie.appserver.service.model.Event;
+
+import java.util.List;
+
+public class UserResponse{
+    @JsonProperty("username")
+    private String userName;
+
+    @JsonProperty("eventsList")
+    private List<Event> eventsList;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @JsonProperty("notifications")
+    private List<String> notifications;
+
+    @JsonProperty("userType")
+    private String userType;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Event> getEventsList() {
+        return eventsList;
+    }
+
+    public void setEventsList(List<Event> eventsList) {
+        this.eventsList = eventsList;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<String> notifications) {
+        this.notifications = notifications;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
 }
