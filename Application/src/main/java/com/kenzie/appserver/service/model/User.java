1d67b0236c0d533be971f9df84b7f38689fbf733
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2023-12-11 00:09:15 -0500

    Updated User

--- Application/src/main/java/com/kenzie/appserver/service/model/User.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/User.java
@@ -2,22 +2,29 @@ package com.kenzie.appserver.service.model;
 
 import java.util.List;
 
-public class User {
+public class User{
 
     private final String userID;
-
     private final String userName;
-
     private final String password;
+    private final List<String> eventsList;
+    private final String email;
+    private final String firstName;
+    private final String lastName;
+    private final List<String> notifications;
+    private final String userType;
 
-    private final List<Event> eventsList;
 
-
-    public User(String userID, String userName, String password, List<Event> eventsList) {
+    public User(String userID, String userName, String password, List<String> eventsList, String email, String firstName, String lastName, List<String> notifications, String userType) {
         this.userID = userID;
         this.userName = userName;
         this.password = password;
         this.eventsList = eventsList;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.notifications = notifications;
+        this.userType = userType;
     }
 
     public String getUserID(){
@@ -32,9 +39,27 @@ public class User {
         return password;
     }
 
-    public List<Event> getEventsList() {
+    public List<String> getEventsList() {
         return eventsList;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public List<String> getNotifications(){
+        return notifications;
+    }
+
+    public String getUserType(){
+        return userType;
+    }
 }
