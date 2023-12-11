eeab423182ea2cc68e037bec5023267222f8aafb
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2023-12-11 00:31:03 -0500

    Updated EventCreateRequest

--- Application/src/main/java/com/kenzie/appserver/controller/model/EventCreateRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/EventCreateRequest.java
@@ -1,34 +1,42 @@
 package com.kenzie.appserver.controller.model;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.constraints.NotEmpty;
-import java.sql.Timestamp;
+
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.List;
+
 
 public class EventCreateRequest{
 
-    @NotEmpty
-    @JsonProperty("eventID")
-    private String eventID;
-    @NotEmpty
+
+
     @JsonProperty("name")
     private String name;
-    @NotEmpty
+
     @JsonProperty("location")
     private String location;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-    @JsonProperty("date")
-    private LocalDateTime date;
+    @JsonProperty("startTime")
+    private LocalDateTime startTime;
 
 
-    public String getEventID(){
-        return eventID;
-    }
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    @JsonProperty("endTime")
+    private LocalDateTime endTime;
+
+    @JsonProperty("peopleAttending")
+    private List<String> peopleAttending;
+
+    @JsonProperty("peopleAttended")
+    private List<String> peopleAttended;
+
+    @JsonProperty("eventSponsor")
+    private String eventSponsor;
+
+
+
 
     public String getName() {
         return name;
@@ -39,12 +47,24 @@ public class EventCreateRequest{
     }
 
 
-    public LocalDateTime getDate() {
-        return date;
+    public LocalDateTime getStartTime() {
+        return startTime;
     }
 
-    public void setEventID(String eventID) {
-        this.eventID = eventID;
+    public LocalDateTime getEndTime(){
+        return endTime;
+    }
+
+    public List<String> getPeopleAttending() {
+        return peopleAttending;
+    }
+
+    public List<String> getPeopleAttended() {
+        return peopleAttended;
+    }
+
+    public String getEventSponsor() {
+        return eventSponsor;
     }
 
     public void setName(String name) {
@@ -55,9 +75,24 @@ public class EventCreateRequest{
         this.location = location;
     }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
     }
 
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
 
+    public void setPeopleAttending(List<String> peopleAttending) {
+        this.peopleAttending = peopleAttending;
+    }
+
+    public void setPeopleAttended(List<String> peopleAttended) {
+        this.peopleAttended = peopleAttended;
+    }
+
+    public void setEventSponsor(String eventSponsor) {
+        this.eventSponsor = eventSponsor;
+    }
 }
+
