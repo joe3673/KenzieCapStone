f20a9199fc1ff6cf51853193bea55ae18f33bb16
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2023-12-11 00:25:23 -0500

    Updated EventRecord

--- Application/src/main/java/com/kenzie/appserver/repositories/model/EventRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/EventRecord.java
@@ -3,60 +3,103 @@ package com.kenzie.appserver.repositories.model;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 
-import java.sql.Timestamp;
+
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.List;
 
 public class EventRecord{
 
-    private String eventID;
+    private final String eventID;
     private String name;
     private String location;
-    private LocalDateTime date;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private List<String> peopleAttending;
+
+    private List<String> peopleAttended;
+
+    private  String eventSponsor;
 
 
-
-
-    public EventRecord(String eventID, String name, String location, LocalDateTime date, Timestamp timestamp) {
+    public EventRecord(String eventID, String name, String location, LocalDateTime startTime, LocalDateTime endTime, List<String> peopleAttending, List<String> peopleAttended, String eventSponsor){
         this.eventID = eventID;
         this.name = name;
         this.location = location;
-        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.peopleAttending = peopleAttending;
+        this.peopleAttended = peopleAttended;
+        this.eventSponsor = eventSponsor;
     }
+
     @DynamoDBHashKey(attributeName = "ID")
-    public String getEventID(){
+    public String getEventID() {
         return eventID;
     }
+
     @DynamoDBAttribute(attributeName = "Name")
     public String getName() {
         return name;
-    }
-    @DynamoDBAttribute(attributeName = "Location")
-    public String getLocation() {
-        return location;
-    }
-
-    @DynamoDBAttribute(attributeName = "Date")
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-
-
-    public void setEventID(String eventID) {
-        this.eventID = eventID;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    @DynamoDBAttribute(attributeName = "Location")
+    public String getLocation() {
+        return location;
+    }
+
     public void setLocation(String location) {
         this.location = location;
     }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
+    @DynamoDBAttribute(attributeName = "Start_Time")
+    public LocalDateTime getStartTime() {
+        return startTime;
     }
 
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    @DynamoDBAttribute(attributeName = "End_Time")
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @DynamoDBAttribute(attributeName = "People_Attending")
+    public List<String> getPeopleAttending() {
+        return peopleAttending;
+    }
+
+    public void setPeopleAttending(List<String> peopleAttending) {
+        this.peopleAttending = peopleAttending;
+    }
+
+    @DynamoDBAttribute(attributeName = "People_Attended")
+    public List<String> getPeopleAttended() {
+        return peopleAttended;
+    }
+
+    public void setPeopleAttended(List<String> peopleAttended) {
+        this.peopleAttended = peopleAttended;
+    }
+
+    @DynamoDBAttribute(attributeName = "Event_Sponsor")
+    public String getEventSponsor() {
+        return eventSponsor;
+    }
+
+    public void setEventSponsor(String eventSponsor) {
+        this.eventSponsor = eventSponsor;
+    }
 }
