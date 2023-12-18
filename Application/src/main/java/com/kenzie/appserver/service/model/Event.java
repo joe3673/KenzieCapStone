45440588ab87ac557bf6891cdcc2e9205a4f4c65
Brynn Rizzo <brynn.rizzo@mykenzie.snhu.edu>
2023-12-17 18:46:43 -0900

    Hopefully fixed the merge conflict

--- Application/src/main/java/com/kenzie/appserver/service/model/Event.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Event.java
@@ -1,15 +1,5 @@
 package com.kenzie.appserver.service.model;
 
 public class Event {
-    /*Event
-    String id
-    String name
-    String startTime
-    String endTime
-    String eventLocation
-    String eventSponsor
-    Array[String] peopleAttending
-    Array[String] peopleAttended
 
-     */
 }
