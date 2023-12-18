ac70d5b281b8f9bc2522920ca0cd494c3dfbeeeb
Brynn Rizzo <brynn.rizzo@mykenzie.snhu.edu>
2023-12-17 18:26:06 -0900

    Progress on front end pages and util

--- Application/src/main/java/com/kenzie/appserver/service/model/Event.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Event.java
@@ -1,4 +1,15 @@
 package com.kenzie.appserver.service.model;
 
 public class Event {
+    /*Event
+    String id
+    String name
+    String startTime
+    String endTime
+    String eventLocation
+    String eventSponsor
+    Array[String] peopleAttending
+    Array[String] peopleAttended
+
+     */
 }
--- Frontend/src/pages/AccountCreation.js
+++ Frontend/src/pages/AccountCreation.js
@@ -5,26 +5,45 @@ import UserClient from "../api/UserClient";
 /*
  AccountCreation. Handles javascript for the
  account creation page.
+
+ Creating a user
  */
 
 class AccountCreation extends BaseClass {
-
     constructor() {
         super();
-        this.bindClassMethods([], this);
+        this.bindClassMethods(['submitUser'], this);
         this.dataStore = new DataStore();
     }
-
 
     async mount() {
         this.client = new UserClient();
     }
 
+    //Function to handle user submission
+    submitUser() {
+        const firstName = document.getElementById('create-user-firstName').value;
+        const lastName = document.getElementById('create-user-lastName').value;
+        const userName = document.getElementById('create-user-userName').value;
+        const email = document.getElementById('create-user-email').value;
+        const password = document.getElementById('create-user-password').value;
+        const userType = document.getElementById('create-user-userType').value;
+
+
+
+        // **Probably need to update this later - Submit the user data to the server using UserClient
+        this.client.createUser({
+            firstName,
+            lastName,
+            userName,
+            email,
+            password,
+            userType,
+        });
+    }
 }
 
-/**
- * Main method to run when the page contents have loaded.
- */
+//Main method to run when the page contents have loaded
 const main = async () => {
     const accountCreation = new AccountCreation();
     accountCreation.mount();
