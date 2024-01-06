9f8c8d0ba387eea726607b7edf7900be619cc48f
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2024-01-06 00:21:45 -0500

    Add files via upload

---
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/QueryUtility.java
@@ -0,0 +1,48 @@
+package com.kenzie.appserver.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kenzie.appserver.controller.model.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+public class QueryUtility{
+
+
+    public class EventControllerClient{
+        public ResultActions getEventById(String id) throws Exception{
+            return mvc.perform(get("/Event/{eventId}", id).accept(MediaType.APPLICATION_JSON));
+        }
+
+        public ResultActions getAllEvents() throws Exception{
+            return mvc.perform(get("/Event/all").accept(MediaType.APPLICATION_JSON));
+        }
+        public ResultActions addNewEvent(EventCreateRequest eventCreateRequest) throws Exception {
+            return mvc.perform(post("/Event/").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(eventCreateRequest)));
+        }
+        public ResultActions deleteEventById(String id) throws Exception{
+            return mvc.perform(delete("/Event/{eventId}", id).accept(MediaType.APPLICATION_JSON));
+        }
+
+
+    }
+
+    public EventControllerClient eventControllerClient;
+    private final MockMvc mvc;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public QueryUtility(MockMvc mvc) {
+        this.mvc = mvc;
+        this.eventControllerClient = new EventControllerClient();
+    }
+
+
+
+}
+
+
