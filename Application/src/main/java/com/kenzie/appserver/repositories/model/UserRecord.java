05c7638cbf541ed3e9eb1226c8f478e4baa47209
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2024-01-06 00:46:33 -0500

    Updated UserRecord

--- Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/UserRecord.java
@@ -2,11 +2,13 @@ package com.kenzie.appserver.repositories.model;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@DynamoDBTable(tableName = "Users")
 public class UserRecord{
     private final String userID;
     private String userName;
@@ -135,4 +137,3 @@ public class UserRecord{
     }
 
 }
-
