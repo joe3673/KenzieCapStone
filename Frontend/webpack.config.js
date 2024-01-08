44136d1046af6cb0538fa347b0b93089c04a99b2
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2024-01-08 16:02:43 -0500

    Updated webpack.config

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -9,8 +9,14 @@ module.exports = {
     usedExports: true
   },
   entry: {
-    examplePage: path.resolve(__dirname, 'src', 'pages', 'examplePage.js'),
-
+    Index: path.resolve(__dirname, 'src', 'pages', 'Index.js'),
+    AccountCreation: path.resolve(__dirname, 'src', 'pages', 'AccountCreation.js'),
+    AccountSignIn: path.resolve(__dirname, 'src', 'pages', 'AccountSignIn.js'),
+    EventPageOrganizer: path.resolve(__dirname, 'src', 'pages', 'EventPageOrganizer.js'),
+    EventViewPageUser: path.resolve(__dirname, 'src', 'pages', 'EventViewPageUser.js'),
+    EventViewPageVisitor: path.resolve(__dirname, 'src', 'pages', 'EventViewPageVisitor.js'),
+    FriendPage: path.resolve(__dirname, 'src', 'pages', 'FriendPage.js'),
+    UserProfile: path.resolve(__dirname, 'src', 'pages', 'UserProfile.js')
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,7 +29,8 @@ module.exports = {
     proxy: [
       {
         context: [
-          '/example',
+          '/User',
+          '/Event'
         ],
         target: 'http://localhost:5001'
       }
@@ -34,6 +41,41 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html',
       inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/AccountCreation.html',
+      filename: 'AccountCreation.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/AccountSignIn.html',
+      filename: 'AccountSignIn.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/EventOrganizer.html',
+      filename: 'EventOrganizer.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/EventViewUser.html',
+      filename: 'EventViewUser.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/EventViewVisitor.html',
+      filename: 'EventViewVisitor.html',
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/FriendsPage.html',
+      filename: 'FriendsPage.html',
+      inject: false
+    }),
+     new HtmlWebpackPlugin({
+       template: './src/UserProfile.html',
+       filename: 'UserProfile.html',
+       inject: false
     }),
      /*new HtmlWebpackPlugin({
           template: './src/index.html',//change to my page - do this for every page, yes
