c48d04b2d64f8d7e268e2c9af4926b426dce4f1d
jordanburkecs <123501150+jordanburkecs@users.noreply.github.com>
2024-01-07 21:23:51 -0500

    Updated CacheClient

--- Application/src/main/java/com/kenzie/appserver/service/CacheClient.java
+++ Application/src/main/java/com/kenzie/appserver/service/CacheClient.java
@@ -5,15 +5,65 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.inject.Inject;
 
 public class CacheClient {
+
+    private class Cache{
+
+        ConcurrentHashMap<String, String> cacheMap;
+        ConcurrentHashMap<String, LocalDateTime> expiryMap;
+
+        int timeInCacheBySeconds;
+        public Cache(){
+            cacheMap = new ConcurrentHashMap<>();
+            expiryMap = new ConcurrentHashMap<>();
+            timeInCacheBySeconds = 1000;
+        }
+
+        public Cache(int seconds){
+            cacheMap = new ConcurrentHashMap<>();
+            expiryMap = new ConcurrentHashMap<>();
+            timeInCacheBySeconds = seconds;
+        }
+
+        public void setValue(String key, String value){
+            cacheMap.put(key, value);
+            expiryMap.put(key, LocalDateTime.now());
+        }
+
+        public Optional<String> getValue(String key){
+            return Optional.of(cacheMap.get(key));
+        }
+
+        public void invalidate(String key){
+            cacheMap.remove(key);
+            expiryMap.remove(key);
+        }
+
+        //Would be called periodically in a real application.
+        public void cleanup(){
+            Iterator<Map.Entry<String, LocalDateTime>> iterator = expiryMap.entrySet().iterator();
+            while(iterator.hasNext()){
+                Map.Entry<String, LocalDateTime> entry = iterator.next();
+                if(LocalDateTime.now().isAfter(entry.getValue().plusSeconds(timeInCacheBySeconds))){
+                    iterator.remove();
+                    cacheMap.remove(entry.getKey());
+                }
+            }
+        }
+
+    }
     private final JedisPool pool;
-    
+
     private Jedis jedi = new Jedis();
 
-
+    Cache cache = new Cache();
 
     public CacheClient() {
         this.pool = new JedisPool(new JedisPoolConfig(), System.getenv("JEDIS_URL"), 6379, 20000);
@@ -37,10 +87,17 @@ public class CacheClient {
      * @param seconds The number of seconds during which the item is available
      * @param value   String representing the value set in the cache
      */
-    public void setValue(String key, int seconds, String value){
+    public void setValue(String key, long seconds, String value){
         checkForNullKey(key);
-        jedi.setex(key, seconds, value);
-
+        try {
+            jedi.setex(key, seconds, value);
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        finally{
+            cache.setValue(key, value);
+        }
     }
 
     /**
@@ -53,14 +110,20 @@ public class CacheClient {
      */
     public Optional<String> getValue(String key){
         checkForNullKey(key);
+        try {
             String value = jedi.get(key);
-            if(value == null){
+            if (value == null) {
                 return Optional.empty();
-            }
-            else{
+            } 
+            else {
                 return Optional.of(value);
             }
-        
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return cache.getValue(key);
+
     }
 
     /**
@@ -73,13 +136,18 @@ public class CacheClient {
      */
     public boolean invalidate(String key) {
         checkForNullKey(key);
-
+        try {
             if (jedi.get(key) != null) {
                 jedi.del(key);
                 pool.close();
                 return true;
             }
             return false;
-        
+        } 
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        cache.invalidate(key);
+        return true;
     }
 }
